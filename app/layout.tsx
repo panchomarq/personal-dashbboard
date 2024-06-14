ad71274332ce679adae17870e2f8b1c3279947c6
Francisco Márquez <franciscomarquez2791@gmail.com>
2024-06-14 09:49:49 -0300

    feat: added logic to sidebar and logic

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={montserrat.className}>
+    <html lang="en" className={`${montserrat.className} antialiased`} >
       <body>{children}</body>
     </html>
   );
