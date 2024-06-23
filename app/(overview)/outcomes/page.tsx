834e603b0a60a2991310888dbc27cbfc2fa57524
Francisco Márquez <franciscomarquez2791@gmail.com>
2024-06-23 18:37:09 -0300

    feat: added new styles to the navbar

--- app/(overview)/outcomes/page.tsx
+++ app/(overview)/outcomes/page.tsx
@@ -1,10 +1,12 @@
 "use client"
+import  IncomeForm  from '@/components/ui/IncomeForm';
 
 export default function page() {
   return (
     <main>
       <h1 className="mb-4 text-xl md:text-2xl"></h1>
       <div>
+        <IncomeForm clients={undefined} />
       </div>
     </main>
   );
--- auth.ts
+++ auth.ts
@@ -1,11 +1,11 @@
 import NextAuth from 'next-auth';
-import Credentials from 'next-auth/providers/credentials';
+import CredentialsProvider from 'next-auth/providers/credentials';
 import { authConfig } from './auth.config';
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
-import type { User } from '@/lib/types/definitions';
 import bcrypt from 'bcrypt';
- 
+import type { User } from '@/lib/types/definitions';
+
 async function getUser(email: string): Promise<User | undefined> {
   try {
     const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
@@ -15,17 +15,16 @@ async function getUser(email: string): Promise<User | undefined> {
     throw new Error('Failed to fetch user.');
   }
 }
- 
-export const { auth, signIn, signOut } = NextAuth({
+
+const options = {
   ...authConfig,
   providers: [
-    Credentials({
+    CredentialsProvider({
       async authorize(credentials) {
         const parsedCredentials = z
           .object({ email: z.string().email(), password: z.string().min(6) })
           .safeParse(credentials);
-        console.log(parsedCredentials)
-        
+
         if (parsedCredentials.success) {
           const { email, password } = parsedCredentials.data;
           const user = await getUser(email);
@@ -37,10 +36,11 @@ export const { auth, signIn, signOut } = NextAuth({
           }
         }
 
-        console.log("invalid credentials")
-        return null
- 
+        console.log("invalid credentials");
+        return null;
       },
     }),
   ],
-});
+};
+
+export default NextAuth(options);
